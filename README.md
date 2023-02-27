53f575823a8c237dabaefcf357875094522101a7
Ben Chavez <ben.m.chavez@gmail.com>
2023-02-27 16:27:06 -0600

    tabs-manager

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tabs-manager-chrome-extension
+
+A Chrom Extension tabs manager to organize your Chrome extension and Chrome Web store documentation tabs.
---
+++ popup.js
@@ -0,0 +1,38 @@
+const tabs = await chrome.tabs.query({
+  url: [
+    'https://developer.chrome.com/docs/webstore/*',
+    'https://developer.chrome.com/docs/extensions/*',
+  ],
+});
+
+// Sort tab names (the titles of the contained HTML pages) alphabetically.
+const collator = new Intl.Collator();
+tabs.sort((a, b) => collator.compare(a.title, b.title));
+
+const template = document.getElementById('li_template');
+const elements = new Set();
+for (const tab of tabs) {
+  const element = template.content.firstElementChild.cloneNode(true);
+
+  const title = tab.title.split('-')[0].trim();
+  const pathname = new URL(tab.url).pathname.slice('/docs'.length);
+
+  element.querySelector('.title').textContent = title;
+  element.querySelector('.pathname').textContent = pathname;
+  element.querySelector('a').addEventListener('click', async () => {
+    // need to focus window as well as the active tab
+    await chrome.tabs.update(tab.id, { active: true });
+    await chrome.windows.update(tab.windowId, { focused: true });
+  });
+
+  elements.add(element);
+}
+document.querySelector('ul').append(...elements);
+
+// Group all the tabs using tabs.group() and move them into the current window.
+const button = document.querySelector('button');
+button.addEventListener('click', async () => {
+  const tabIds = tabs.map(({ id }) => id);
+  const group = await chrome.tabs.group({ tabIds });
+  await chrome.tabGroups.update(group, { title: 'DOCS' });
+});
